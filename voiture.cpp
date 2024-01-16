fa2b8b18dc59d2c78478878445e40e937052579f
gaelced <144811508+gaelced@users.noreply.github.com>
2024-01-16 09:55:51 +0100

    Create voiture.cpp

---
+++ voiture.cpp
@@ -0,0 +1,39 @@
+#include <string>
+#include <iostream>
+#include "classe.hpp"
+
+using namespace std;
+
+Voiture::Voiture() {
+    this->vitesse=0;
+    cout << "Un objet a été créé" << endl;
+}
+
+Voiture::~Voiture() {
+    cout << "Un objet a été détruit" << endl;
+}
+
+void Voiture::setMarque(string marq) {
+    this->marque = marq;
+}
+
+void Voiture::setCouleur(string coul) {
+    this->couleur = coul;
+}
+
+void Voiture::setVitesse(int vit) {
+    this->vitesse = vit;
+}
+
+string Voiture::getCouleur() {
+    return this->couleur;
+}
+
+string Voiture::getMarque() {
+    return this->marque;
+}
+
+int Voiture::getVitesse() {
+    return this->vitesse;
+}
+
